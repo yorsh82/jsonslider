b45ea5d8568290b722e9ef0d0573722e62332983
dcdeiv <davide.dicriscito@gmail.com>
2015-04-30 15:23:02 +0200

    init

---
+++ jquery.jsonslider.js
@@ -0,0 +1,53 @@
+(function($) {
+	$.fn.jsonSlider = function(options) {
+		var def = {
+				json: undefined,
+				active: 'slider-active'
+			},
+			cfg = $.extend(true, def, options),
+			store = cfg.json,
+			$wrap = $(this);
+
+		$.getJSON(store, function(data) {
+			var figs, first, i,
+				arr = $.map(data, function(el) {
+					return el;
+				});
+
+			for (i = 0; i < arr.length; i++) {
+				$wrap.append($('<figure><img src="' + arr[i].url + '" alt="' + arr[i].alt + '"/></figure>'));
+			}
+
+			figs = $wrap.children();
+			first = figs.first();
+
+			first.addClass('slider-active');
+
+			figs.not(first).hide();
+
+			function slider() {
+				var $next,
+					$active = $('.slider-active');
+
+				if ($active.next().length === 0) {
+					$next = figs.first();
+				} else {
+					$next = $active.next();
+				}
+
+				$active.fadeOut(1000, function() {
+					$(this).removeClass('slider-active');
+				});
+				$next.fadeIn(1000, function() {
+					$(this).addClass('slider-active');
+				});
+			}
+
+			setTimeout(
+				setInterval(function() {
+					slider();
+				}, 4000), 5000);
+
+		});
+	};
+})(jQuery);
