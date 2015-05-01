1083b6b929f29319a62346863024dd2505d3abe0
dcdeiv <davide.dicriscito@gmail.com>
2015-05-01 17:58:00 +0200

    updated to v0.2.0

--- bower/jsonslider/README.md
+++ bower/jsonslider/README.md
@@ -9,6 +9,11 @@ JSONslider is a simple jQuery slider for pictures stored dynamically in a JSON f
 
 **This is a beta version, it has not been fully tested**
 
+## DOWNLOAD
+
+* Git: `$ git https://github.com/dcdeiv/jsonslider.git`
+* Bower: `$ bower install jsonslider`
+
 ## SET UP
 
 ### 1 - Set up the JSON file
@@ -32,12 +37,14 @@ Give a unique ID or a class to an empty `<div>` in your `HTML`:
 ### 3 - Run the function
 This is an example of how the function must be called with all available options.
 
-  $( document ).ready(function() {
-  	$( '.your-class' ).jsonSlider({
-  		json: 'path-to-your-json-file.json',
-  		Class: 'slider-active' //default class
-  	});
-  });
+    $( document ).ready(function() {
+      	$( '.your-class' ).jsonSlider({
+      		json: 'path-to-your-json-file.json',
+      		Class: 'slider-active' //default class
+      		orientation: 'landscape', //portrait not tested yet
+      		aspectRatio: '16:9' //aspect ratio of the slider. Chose it according to pictures aspect ratio.
+      	});
+    });
 
 For compatibility issues I added the option `Class` that lets you set a custom class to call the *active element*.
 By default the class is `'slider-active`.
--- bower/jsonslider/jquery.jsonslider.js
+++ bower/jsonslider/jquery.jsonslider.js
@@ -1,57 +1,96 @@
-//JSONslider v.0.1.0
+//JSONslider v.0.2.0
 //http://www.github.com/dcdeiv/jsonslider
 // GPLv2 http://www.gnu.org/licenses/gpl-2.0-standalone.html
 (function($) {
-    $.fn.jsonSlider = function(options) {
-        var def = {
-                json: undefined,
-                Class: 'slider-active'
-            },
-            cfg = $.extend(true, def, options),
-            store = cfg.json,
-            active = cfg.Class,
-            $wrap = $(this);
+	$.fn.jsonSlider = function(options) {
+		var $wrapW, newHeight, wrap,
+			def = {
+				json: undefined,
+				Class: 'slider-active',
+				orientation: 'landscape', //'portrait'
+				aspectRatio: '16:9'
+			},
+			cfg = $.extend(true, def, options),
+			store = cfg.json,
+			active = cfg.Class,
+			orient = cfg.orientation,
+			aspect = cfg.aspectRatio,
+			$wrap = $(this),
+			AR = aspect.split(':'),
+			ARx = parseInt(AR[0]),
+			ARy = parseInt(AR[1]),
+			arI = ARx / ARy;
 
-        $.getJSON(store, function(data) {
-            var figs, first, i,
-                arr = $.map(data, function(el) {
-                    return el;
-                });
+		$wrapW = $wrap.width();
 
-            for (i = 0; i < arr.length; i++) {
-                $wrap.append($('<figure><img src="' + arr[i].url + '" alt="' + arr[i].alt + '"/></figure>'));
-            }
+		$wrap.append('<div>');
+		wrap = $wrap.children();
 
-            figs = $wrap.children();
-            first = figs.first();
+		$(window).resize(function() {
+			if ('landscape' === orient) {
+				newHeight = $wrapW / arI;
 
-            first.addClass(active);
+				$wrap.height(newHeight);
+			} else if ('portrait' === orient) {
+				newHeight = $wrapW * arI;
 
-            figs.not(first).hide();
+				$wrap.height(newHeight);
+			}
 
-            function slider(e) {
-                var $next,
-                    $active = $('.' + e);
+			wrap.css({
+				position: 'relative',
+				width: $wrapW,
+				minHeight: newHeight,
+				margin: '0 auto',
+				padding: 0,
+				backgroundColor: 'inherit',
+				overflow: 'hidden'
+			});
+		});
 
-                if ($active.next().length === 0) {
-                    $next = figs.first();
-                } else {
-                    $next = $active.next();
-                }
+		$.getJSON(store, function(data) {
+			var $figs, first, i,
+				arr = $.map(data, function(el) {
+					return el;
+				});
 
-                $active.fadeOut(1000, function() {
-                    $(this).removeClass(e);
-                });
-                $next.fadeIn(1000, function() {
-                    $(this).addClass(e);
-                });
-            }
+			for (i = 0; i < arr.length; i++) {
+				wrap.append($('<figure><img src="' + arr[i].url + '" alt="' + arr[i].alt + '"/></figure>'));
+			}
 
-            setTimeout(
-                setInterval(function() {
-                    slider(active);
-                }, 4000), 5000);
+			$figs = wrap.children();
 
-        });
-    };
+			$figs.children().height('height', newHeight);
+
+			first = $figs.first();
+
+			first.addClass(active);
+
+			$figs.not(first).hide();
+
+			function slider(e) {
+				var $next,
+					$active = $('.' + e);
+
+				if ($active.next().length === 0) {
+					$next = $figs.first();
+				} else {
+					$next = $active.next();
+				}
+
+				$active.fadeOut(1000, function() {
+					$(this).removeClass(e);
+				});
+				$next.fadeIn(1000, function() {
+					$(this).addClass(e);
+				});
+			}
+
+			setTimeout(setInterval(function() {
+				slider(active);
+			}, 4000), 5000);
+
+			$(window).resize();
+		});
+	};
 })(jQuery);
